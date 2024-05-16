12ce08aa3d6526e680f452071a7f78977a0f6a51
luisvazquez <luis_v98@outlook.com>
2024-05-16 10:20:32 -0500

    Updated

--- PCA.py
+++ PCA.py
@@ -11,46 +11,51 @@ from sklearn.base import BaseEstimator
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import keras
 from keras.datasets import cifar10
 
 
-#load dataset from cifar with keras library and load the dataset and store in keras directory
-pic_class = keras.datasets.cifar10
-(x_train, y_train), (x_test, y_test) = pic_class.load_data()
+# Load CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-#print the shape of training, testing, and label data
-# print('Training Data Shape: ', x_train.shape)
-# print('Testing Data Shape: ', x_test.shape)
-#
-# print('Label Training Data Shape: ', y_train.shape)
-# print('Label Testing Data Shape: ', y_test.shape)
+# Preprocess data - flatten images and normalize data
+x_train_flat = x_train.reshape(x_train.shape[0], -1) / 255.0
+x_test_flat = x_test.reshape(x_test.shape[0], -1) / 255.0
 
 
-#normalize pixels between 0 and 1
-x_train = x_train/255.0
-
-
-
-#flatten images
-x_train_flat = x_train.reshape(-1,3072)
-feat_cols = ['pixel' + str(i) for i in range(x_train_flat.shape[1])]
-df_cifar = pd.DataFrame(x_train_flat, columns = feat_cols)
-df_cifar['Label'] = y_train
-
-#create PCA method
-# pca_cifar = PCA(n_components = 2)
-# principalComponents_cifar = pca_cifar.fit_transform(df_cifar.iloc[:, :-1])
-#
-# #convert principal components
-# principal_cifar_Df = pd.DataFrame(data = principalComponents_cifar,
-#                                   columns = ['Principal Component 1', 'Principal Component 2'])
-# principal_cifar_Df['Label'] = y_train
-# principal_cifar_Df.head()
-
-
+# PCA
 pca = PCA(0.99)
 pca.fit(x_train_flat)
-PCA(copy = True, iterated_power = 'auto', n_components = 0.99, random_state = None, svd_solver = 'auto', tol = 0.0, whiten = False)
+x_train_flt = pca.transform(x_train_flat)
+
+
+# Decision Tree Model
+DT = DecisionTreeClassifier()
+history = DT.fit(x_train_flat, y_train)
+
+# HS Model
+HS = HSTreeClassifierCV()
+history = HS.fit(x_train_flat, y_train)
+
+
+# Predictions
+DT_predictions = DT.predict(x_test_flat)
+HS_predictions = HS.predict(x_test_flat)
+
+
+# Calculate accuracy
+DT_accuracy = accuracy_score(y_test, DT_predictions)
+HS_accuracy = accuracy_score(y_test, HS_predictions)
+print("Decision Tree Accuracy:", DT_accuracy)
+print("Hierarchical Shrinkage Accuracy:", HS_accuracy)
+
+
+
+
+
+
+
+
 
 
 # Plotting cumulative explained variance ratio
@@ -62,32 +67,3 @@ plt.ylabel('Cumulative Explained Variance')
 plt.grid(True)
 plt.savefig('PCA')
 plt.show()
-
-
-
-
-
-
-# Load CIFAR-10 dataset
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
-
-# Preprocess data - flatten images and normalize data
-x_train_flat = x_train.reshape(x_train.shape[0], -1) / 255.0
-x_test_flat = x_test.reshape(x_test.shape[0], -1) / 255.0
-
-# Decision Tree Model
-model = DecisionTreeClassifier()
-history = model.fit(x_train_flat, y_train)
-
-# HS Model
-model = HSTreeClassifierCV()
-history = model.fit(x_train_flat, y_train)
-
-
-# Predictions
-predictions = model.predict(x_test_flat)
-
-# Calculate accuracy
-accuracy = accuracy_score(y_test, predictions)
-print("Accuracy:", accuracy)

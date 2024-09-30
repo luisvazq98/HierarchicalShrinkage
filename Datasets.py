903cbcb6cbc39420dd168f4b3a0b733194c7e431
luisvazq98 <luis_v98@outlook.com>
2024-09-30 18:30:04 -0500

    Updated

--- Datasets.py
+++ Datasets.py
@@ -6,7 +6,6 @@
 
 import tensorflow as tf
 from tensorflow.keras.datasets import cifar10, fashion_mnist
-import tensorflow_datasets as tfds
 from sklearn.datasets import load_iris
 from sklearn import datasets
 import matplotlib.pyplot as plt
@@ -14,6 +13,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.metrics import accuracy_score
+import time
 
 TITANIC_URL = 'https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv'
 CREDIT_URL = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default%20of%20credit%20card%20clients.xls'
@@ -42,7 +42,7 @@ def load_data_image(dataset):
 
     return x_train_images, y_train_images, x_test_images, y_test_images
 
-dataset, info = tfds.load('oxford_iiit_pet', split=['train[:80%]', 'train[80%:]'], with_info=True, as_supervised=True)
+#dataset, info = tfds.load('oxford_iiit_pet', split=['train[:80%]', 'train[80%:]'], with_info=True, as_supervised=True)
 
 ###############################################
 #
@@ -51,17 +51,10 @@ dataset, info = tfds.load('oxford_iiit_pet', split=['train[:80%]', 'train[80%:]'
 ###############################################
 
 ###### SPLITTING DATASETS (80 TRAINING, 20 TESTING) ######
-train_dataset = dataset[0]
-test_dataset = dataset[1]
-
 def split_data(x, y):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
     return x_train, x_test, y_train, y_test
-
-###### PCA ######
-
-
 
 
 
@@ -72,30 +65,71 @@ def split_data(x, y):
 ###############################################
 
 ###### LOADING DATA AND PROCESSING DATA ######
-x_train_images, y_train_images, x_test_images, y_test_images = load_data_image(cifar10)
+# CIFAR-10
+x_train_cifar, y_train_cifar, x_test_cifar, y_test_cifar = load_data_image(cifar10)
 
 # Flatten images
-x_train_images_flat = x_train_images.reshape(x_train_images.shape[0], -1)
-x_test_images_flat = x_test_images.reshape(x_test_images.shape[0], -1)
+x_train_cifar_flat = x_train_cifar.reshape(x_train_cifar.shape[0], -1)
+x_test_cifar_flat = x_test_cifar.reshape(x_test_cifar.shape[0], -1)
 
 # Normalize images
-x_train_images_flat = x_train_images_flat / 255.0
-x_test_images_flat = x_test_images_flat / 255.0
+x_train_cifar_flat = x_train_cifar_flat / 255.0
+x_test_cifar_flat = x_test_cifar_flat / 255.0
+
+
+# FASHION-MINST
+x_train_fashion, y_train_fashion, x_test_fashion, y_test_fashion = load_data_image(fashion_mnist)
+
+# Flatten images
+x_train_fashion_flat = x_train_fashion.reshape(x_train_fashion.shape[0], -1)
+x_test_fashion_flat = x_test_fashion.reshape(x_test_fashion.shape[0], -1)
+
+# Normalize images
+x_train_fashion_flat = x_train_fashion_flat / 255.0
+x_test_fashion_flat = x_test_fashion_flat / 255.0
+
 
 
 ###### SPLITTING DATA ######
 
 
 
+###### PCA ######
+
+
+
 
 ###### DT ######
 clf = DecisionTreeClassifier()
-clf.fit(x_train_images_flat, y_train_images.ravel())
 
-y_pred = clf.predict(x_test_images_flat)
-accuracy = accuracy_score(y_test_images, y_pred)
+# CIFAR-10
+start_time = time.time()
+clf.fit(x_train_cifar_flat, y_train_cifar.ravel())
+end_time = time.time()
+
+# FASHION-MINST
+start_time = time.time()
+clf.fit(x_train_fashion_flat, y_train_fashion.ravel())
+end_time = time.time()
+
+# IRIS
+
+# TITANIC
+
+# OXFORD PETS
+
+# CREDIT CARD
+
+
+# Calculate the elapsed time
+training_time = end_time - start_time
+print(f"Time taken to train the model: {training_time:.2f} seconds")
+
+y_pred = clf.predict(x_test_fashion_flat)
+accuracy = accuracy_score(y_test_fashion, y_pred)
 print(f"Test accuracy: {accuracy * 100:.2f}%")
-print("hello")
+
+
 
 ###### PCA-DT ######
 

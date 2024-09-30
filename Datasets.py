8c160523455bf1e390f16a4614d8fe27bddbdedf
Luis <luis_v98@outlook.com>
2024-09-30 15:52:20 -0500

    Updated

--- Datasets.py
+++ Datasets.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-
+from sklearn.metrics import accuracy_score
 
 TITANIC_URL = 'https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv'
 CREDIT_URL = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default%20of%20credit%20card%20clients.xls'
@@ -67,11 +67,35 @@ def split_data(x, y):
 
 ###############################################
 #
-# TRAINING OF MODELS
+# DEVELOPMENT AND TRAINING OF MODELS
 #
 ###############################################
 
+###### LOADING DATA AND PROCESSING DATA ######
+x_train_images, y_train_images, x_test_images, y_test_images = load_data_image(cifar10)
+
+# Flatten images
+x_train_images_flat = x_train_images.reshape(x_train_images.shape[0], -1)
+x_test_images_flat = x_test_images.reshape(x_test_images.shape[0], -1)
+
+# Normalize images
+x_train_images_flat = x_train_images_flat / 255.0
+x_test_images_flat = x_test_images_flat / 255.0
+
+
+###### SPLITTING DATA ######
+
+
+
+
 ###### DT ######
+clf = DecisionTreeClassifier()
+clf.fit(x_train_images_flat, y_train_images.ravel())
+
+y_pred = clf.predict(x_test_images_flat)
+accuracy = accuracy_score(y_test_images, y_pred)
+print(f"Test accuracy: {accuracy * 100:.2f}%")
+print("hello")
 
 ###### PCA-DT ######
 

81e64a18cbb94c859a330f1b3559a9cc6a853fae
luisvazquez <luis_v98@outlook.com>
2024-12-03 08:53:31 -0600

    updated

--- Validating_HS.py
+++ Validating_HS.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import numpy as np
+import imodels as im
+import subprocess
 import os
+import json
 import kagglehub
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn import datasets
 from sklearn.model_selection import train_test_split, KFold
 from imodels import HSTreeRegressorCV, HSTreeRegressor, HSTreeClassifier
+from imodels import get_clean_dataset
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 from sklearn.metrics import r2_score, mean_squared_error, roc_auc_score, accuracy_score
 from ucimlrepo import fetch_ucirepo
@@ -16,9 +20,17 @@ from sklearn.model_selection import GridSearchCV
 LEAVES = np.array([2, 4, 8, 12, 15, 20, 24, 28, 30, 32])
 METRIC_AUC = pd.DataFrame(columns=[2, 4, 8, 12, 15, 20, 24, 28, 30, 32])
 METRIC_ACC = pd.DataFrame(columns=[2, 4, 8, 12, 15, 20, 24, 28, 30, 32])
+DATASET = "recidivism"
+SOURCE = "imodels"
 
+# MODELS
 HS = HSTreeClassifier()
 DT = DecisionTreeClassifier()
+
+
+# Getting clean dataset from iModels
+X, Y, features = get_clean_dataset(DATASET, SOURCE, return_target_col_names=True)
+
 
 ################################################
 #
@@ -26,74 +38,86 @@ DT = DecisionTreeClassifier()
 #
 ################################################
 
-######################## DIABETES (REGRESSION) ########################
-diabetes = datasets.load_diabetes()
-X = diabetes.data
-Y = diabetes.target
-
-
-######################## RED WINE ########################
-# Download the dataset
-path = kagglehub.dataset_download("uciml/red-wine-quality-cortez-et-al-2009")
-# Check the files in the directory
-files = os.listdir(path)
-file_path = os.path.join(path, "winequality-red.csv")
-
-# Load the dataset into a DataFrame
-df = pd.read_csv(file_path)
-X = df.drop(columns=['quality'])
-Y = df['quality']
-
-
-
-################################################
+# ######################## DIABETES (REGRESSION) ########################
+# diabetes = datasets.load_diabetes()
+# X = diabetes.data
+# Y = diabetes.target
 #
-# CLASSIFICATION DATASETS
 #
-################################################
+# ######################## RED WINE ########################
+# # Download the dataset
+# path = kagglehub.dataset_download("uciml/red-wine-quality-cortez-et-al-2009")
+# # Check the files in the directory
+# files = os.listdir(path)
+# file_path = os.path.join(path, "winequality-red.csv")
+#
+# # Load the dataset into a DataFrame
+# df = pd.read_csv(file_path)
+# X = df.drop(columns=['quality'])
+# Y = df['quality']
+#
+#
+#
+# ################################################
+# #
+# # CLASSIFICATION DATASETS
+# #
+# ################################################
 
-######################## DIABETES (CLASSIFICATION) ########################
-# Download latest version
-path = kagglehub.dataset_download("mathchi/diabetes-data-set")
-files = os.listdir(path)
-file_path = os.path.join(path, "diabetes.csv")
-
-df = pd.read_csv(file_path)
-X = df.drop(columns=['Outcome'])
-Y = df['Outcome']
+# ######################## DIABETES (CLASSIFICATION) ########################
+# # Download latest version
+# path = kagglehub.dataset_download("mathchi/diabetes-data-set")
+# files = os.listdir(path)
+# file_path = os.path.join(path, "diabetes.csv")
+#
+# df = pd.read_csv(file_path)
+# X = df.drop(columns=['Outcome'])
+# Y = df['Outcome']
 
 
 ######################## BREAST CANCER ########################
-# Fetch dataset
-breast_cancer = fetch_ucirepo(id=14)
-df = breast_cancer.data
+# # Fetch dataset
+# breast_cancer = fetch_ucirepo(id=14)
+# df = breast_cancer.data
+#
+# # Data (as pandas dataframes)
+# X = breast_cancer.data.features
+# Y = breast_cancer.data.targets
+#
+# # Dropping rows with NaN values
+# Y = Y.drop(index=X[X.isna().any(axis=1)].index)
+# X = X.dropna(axis=0)
+#
+# # Reset the indices of X and Y
+# X.reset_index(drop=True, inplace=True)
+# Y.reset_index(drop=True, inplace=True)
+#
+#
+# # Identify and encode categorical columns
+# categorical_columns = X.select_dtypes(include=["object"]).columns
+# label_encoders = {}
+#
+# for col in categorical_columns:
+#     le = OneHotEncoder(sparse_output=False)
+#     transformed = le.fit_transform(X[col].values.reshape(-1, 1))
+#
+#     # Convert transformed data into a DataFrame and join back with X
+#     transformed_df = pd.DataFrame(transformed, columns=le.categories_[0])
+#     X = X.drop(columns=[col]).join(transformed_df, lsuffix='_left', rsuffix='_right')
+#     label_encoders[col] = le
+#
+# # Encode the target variable Y
+# y_label_encoder = LabelEncoder()
+# Y = y_label_encoder.fit_transform(Y)
 
-# Data (as pandas dataframes)
-X = breast_cancer.data.features
-Y = breast_cancer.data.targets
 
-# Identify and encode categorical columns
-categorical_columns = X.select_dtypes(include=["object"]).columns
-label_encoders = {}
-
-for col in categorical_columns:
-    le = LabelEncoder()
-    X.loc[:, col] = le.fit_transform(X[col])  # Use .loc for assignment
-    label_encoders[col] = le
-
-# Encode the target variable Y
-y_label_encoder = LabelEncoder()
-Y = y_label_encoder.fit_transform(Y)
-
-
-######################## HABERMAN ########################
-# fetch dataset
-haberman_s_survival = fetch_ucirepo(id=43)
-
-# data (as pandas dataframes)
-X = haberman_s_survival.data.features
-Y = haberman_s_survival.data.targets
-
+# ######################## HABERMAN ########################
+# # fetch dataset
+# haberman_s_survival = fetch_ucirepo(id=43)
+#
+# # data (as pandas dataframes)
+# X = haberman_s_survival.data.features
+# Y = haberman_s_survival.data.targets
 
 
 ################################################
@@ -188,9 +212,9 @@ plt.plot(LEAVES, data_auc_dt, marker='o', linestyle='-', color='b', label="DT AU
 plt.xlabel("Number of Leaves")
 plt.ylabel("AUC")
 plt.grid(True)
-#plt.title("R2 Score", fontsize=20)
+plt.title("Juvenile", fontsize=20)
 plt.legend()
-#plt.savefig("haberman_class_auc")
+plt.savefig("juvenile_class_auc")
 plt.show()
 
 ######################## ACCURACY ########################
@@ -200,7 +224,7 @@ plt.plot(LEAVES, data_acc_dt, marker='o', linestyle='-', color='b', label='DT AC
 plt.xlabel('Number of Leaves')
 plt.ylabel('Accuracy')
 plt.grid(True)
-#plt.title("Mean Squared Error", fontsize=20)
+plt.title("Juvenile", fontsize=20)
 plt.legend()
-#plt.savefig("haberman_class_acc")
+plt.savefig("juvenile_class_acc")
 plt.show()
